dad617fbd0bea1fd729f90c554082081a61b5bca
Christian Vigh <christian.vigh@orange.fr>
2015-10-29 12:47:28 +0100

    Still under development

--- Javascript/thrak.ui.calculator-1.0.0.js
+++ Javascript/thrak.ui.calculator-1.0.0.js
@@ -4,7 +4,7 @@
         thrak.ui.calculator-1.0.0.js
 
     DESCRIPTION
-        A calculator widget.
+        A calculator widget. *** UNDER DEVELOPMENT ***
 
     AUTHOR
         Christian Vigh, 10/2015.
@@ -16,64 +16,16 @@
  **************************************************************************************************************/
 
 
- ( function ( $ )
+ ( function ( $, script )
    {
 	/*-------------------------------------------------------------------------------------------------------------
 	 *
 	 *  $.calculator -
 	 *	Global calculator management. Can be called with the following arguments :
 	 *
-	 *	$. calculator ( 'layout' ) -
-	 *		Returns the default layout.
-	 *
-	 *	$. calculator ( 'layout', name ) -
-	 *		Returns the structure of the layout specified by 'name'.
-	 *	
-	 *	$. calculator ( 'layout', name, options ) -
-	 *		Defines a new layout. Returns the layout definition (which will be more complete than the
-	 *		supplied options).
-	 *
-	 *	It also contains the following members :
-	 *	. keys -
-	 *		Key identifiers.
-	 *
-	 *	. buttons -
-	 *		Button definitions 
-	 *
 	 *-------------------------------------------------------------------------------------------------------------*/
 	$. calculator	= function ( )
 	   {
-		var	argc	=  arguments. length ;
-
-
-		if  ( ! argc )
-			return ( undefined ) ;
-
-		switch  ( arguments [0]. toLowerCase ( ) )
-		   {
-			case	'layout' :
-			   {
-				var	name	=  'standard' ;
-				var	obj	=  undefined ;
-
-				switch  ( argc )
-				   {
-					case	2 :
-						name	=  arguments [1] ;
-						break ;
-
-					case	3 :
-					default :
-						name	=  arguments [1] ;
-						obj	=  arguments [2] ;
-				    }
-
-				if  ( obj  !==  undefined )
-					__set_layout ( name, obj ) ;
-
-				return ( __get_layout ( name ) ) ;
-			    }
-		    }
 	    }
 
 	$. calculator. keys	=  
@@ -93,7 +45,8 @@
 		ID_DIGIT_C		:  12,
 		ID_DIGIT_D		:  13,
 		ID_DIGIT_E		:  14,
-		ID_DIGIT_F		:  15
+		ID_DIGIT_F		:  15,
+		ID_DOT			:  16
 	    } ;
 
 	$. calculator. buttons	=
@@ -225,6 +178,14 @@
 			virtual		:  false,
 			callback	:  accumulate,
 			class		:  'digit hexadecimal-digit'
+		 },
+		{
+			id		:  $. calculator. keys. ID_DOT,
+			key		:  [ '.', ',' ],
+			label		:  '.',
+			virtual		:  false,
+			callback	:  accumulate,
+			class		:  'digit hexadecimal-digit'
 		 }
 	    ] ;
 
@@ -300,9 +261,6 @@
 				$this. attr ( 'calculator-id', id ) ;
 				me. options. data. icon. attr ( 'calculator-id', id ) ;
 
-				// Install calculator event handlers
-				this. _calculator_events ( me. options. data. calculator ) ;
-
 				// Set current layout
 				this. layout ( me. options. layout ) ;
 			    },
@@ -371,75 +329,44 @@
 				var	me	=  this ;
 
 				if  ( name  ===  undefined )
-					return ( me. options. layout ) ;
+					name	=  me. options. layout ;
 
-				var	layout	=  $. calculator ( 'layout', name ) ;
-
-				me. options. data. calculator. html ( layout. html ( ) ) ;
-
-				// Buttonify the contents
-				var	$buttons		=  $('.button', me. options. data. calculator). button ( ). removeClass ( 'button' ) ;
-
-				// Get the default outer width and height of a button
-				var	$example_button		=  $buttons. first ( ) ;
-				var	button_real_height	=  $example_button. outerHeight ( ),
-					button_real_width	=  $example_button. outerWidth ( ),
-					button_height		=  $example_button. height ( ),
-					button_width		=  $example_button. width ( ),
-					height_delta		=  button_real_width - button_width,
-					width_delta		=  button_real_height - button_height ;
-							
-				// Set cell widths for table cells having a colspan
-				$('[colspan]:not([colspan=""])'). each 
-				   (
-					function ( )
+				if  ( me. options. data. layout  ===  undefined )
+				   {
+					if  ( ! name  in  layouts )
 					   {
-						var	$this		=  $(this) ;
-						var	width		=  $this. width ( ) ;
-						var	colspan		=  parseInt ( $this. attr ( 'colspan' ) ) ;
-						var	new_width	=  ( button_width + width_delta ) * colspan ;
-
-
-						$this. css ( 'width', new_width + 'px' ) ;
-
-						$('.ui-button[colspan]:not([colspan=""])', $this). each
-						   (
-							function ( )
-							   {
-								var	$this_button	=  $(this) ;
-
-								$this_button. css ( 'width', new_width + 'px' ) ;
-							    }
-						    ) ;
+						callback ( { name : name, html : function ( ) { return ( '*** UNDEFINED CALCULATOR LAYOUT ' + name + ' ***' ) ; } } ) ;
+						return ;
 					    }
-				    ) ;
 
-				// Same for rowspan and table cell height
-				$('[rowspan]:not([rowspan=""])'). each 
-				   (
-					function ( )
+					if  ( layouts [ name ]. html  ===  undefined )
 					   {
-						var	$this		=  $(this) ;
-						var	height		=  $this. height ( ) ;
-						var	rowspan		=  parseInt ( $this. attr ( 'rowspan' ) ) ;
-						var	new_height	=  ( button_height + height_delta ) * rowspan ;
+						var	layout	=  layouts [ name ] ;
 
+						layout. name	=  name ;
+			
+						if  ( layout. url  ===  undefined )
+							layout. url	=  script. dirname + '/' + layouts_url + '/' + name + '.html' ;
 
-						$this. css ( 'height', new_height + 'px' ) ;
-
-						$('.ui-button[rowspan]:not([rowspan=""])', $this). each
-						   (
-							function ( )
+						$. ajax
+						   ({
+							url		:  layout. url,
+							async		:  true,
+							dataType	:  'html',
+							success		:  function ( data ) 
 							   {
-								var	$this_button	=  $(this) ;
+								layout. html	=  function ( )
+								   {
+									return  ( data ) ;
+								    }
 
-								$this_button. css ( 'height', new_height + 'px' ) ;
+								new_layout ( me, layout ) ;
 							    }
-						    ) ;
+						     }) ;
 					    }
-				    ) ;
+				    }
+			     },
 
-			    },
 
 			// open -
 			//	Opens the calculator
@@ -460,132 +387,88 @@
 	 *
 	 *-------------------------------------------------------------------------------------------------------------*/
 
+	var	layouts_url	=  'calculator.layouts' ;
 	var	layouts		=
 	   {
 		'standard'		:
 		   {
-			initialized		:  false,
-			definitions		:  
-			   {
-				cols			:  5,
-				rows			:  7,
-				template		:
-					'									\
-						<line>	\
-							<lcd width="5">						\
-						</line>	\
-						<group width="3" class="numeric-keypad">			\
-							<line>						\
-								<button name="digit_7"/>			\
-								<button name="digit_8"/>			\
-								<button name="digit_9"/>			\
-							</line>						\
-							<line>						\
-								<button name="digit_4"/>			\
-								<button name="digit_5"/>			\
-								<button name="digit_6"/>			\
-							</line>						\
-							<line>						\
-								<button name="digit_1"/>			\
-								<button name="digit_2"/>			\
-								<button name="digit_3"/>			\
-							</line>						\
-							<line>						\
-								<button name="digit_0" width="2"/>		\
-							</line>						\
-						</group>							\
-					'
-			    }
 		    }
 	    } ;
 
 
-	function  __get_layout ( name )
+	function  new_layout ( me, layout ) 
 	   {
-		if  ( name  in  layouts )
-		   {
-			if  ( ! layouts [ name ]. initialized )
-				__initialize_layout ( name ) ;
+		// Insert html code for the current layout
+		me. options. data. layout	=  $. extend ( {}, layout ) ;
+		me. options. data. calculator. html ( layout. html ( ) ) ;
 
-			return  ( layouts [ name ]. definitions ) ;
-		    }
-		else 
-			return ( undefined ) ;
-	    }
+		// Install calculator event handlers
+		me. _calculator_events ( me. options. data. calculator ) ;
+
+		// 
+
+		// Buttonify the contents
+		var	$buttons		=  $('.button', me. options. data. calculator). button ( ). removeClass ( 'button' ) ;
+
+		// Get the default outer width and height of a button
+		var	$example_button		=  $buttons. first ( ) ;
+		var	button_real_height	=  $example_button. outerHeight ( ),
+			button_real_width	=  $example_button. outerWidth ( ),
+			button_height		=  $example_button. height ( ),
+			button_width		=  $example_button. width ( ),
+			height_delta		=  button_real_width - button_width,
+			width_delta		=  button_real_height - button_height ;
+					
+		// Set cell widths for table cells having a colspan
+		$('[colspan]:not([colspan=""])', me. options. data. calculator). each 
+		   (
+			function ( )
+			   {
+				var	$this		=  $(this) ;
+				var	width		=  $this. width ( ) ;
+				var	colspan		=  parseInt ( $this. attr ( 'colspan' ) ) ;
+				var	new_width	=  ( button_width + width_delta ) * colspan ;
 
 
-	function  __set_layout ( name, options )
-	   {
-	    }
+				$this. css ( 'width', new_width + 'px' ) ;
+
+				$('.ui-button[colspan]:not([colspan=""])', $this). each
+				   (
+					function ( )
+					   {
+						var	$this_button	=  $(this) ;
+
+						$this_button. css ( 'width', new_width + 'px' ) ;
+					    }
+				    ) ;
+			    }
+		    ) ;
+
+		// Same for rowspan and table cell height
+		$('[rowspan]:not([rowspan=""])', me. options. data. calculator). each 
+		   (
+			function ( )
+			   {
+				var	$this		=  $(this) ;
+				var	height		=  $this. height ( ) ;
+				var	rowspan		=  parseInt ( $this. attr ( 'rowspan' ) ) ;
+				var	new_height	=  ( button_height + height_delta ) * rowspan ;
 
 
-	function  __generate_layout ( layout, $template )
-	   {
-		var	layout_array	=  Array. create ( layout. definitions. rows, layout. definitions. cols ) ;
-		var	html		=  '<table class="ui-calculator-content" cellpadding="0" cellspacing="0">' ;
-	    }
+				$this. css ( 'height', new_height + 'px' ) ;
 
+				$('.ui-button[rowspan]:not([rowspan=""])', $this). each
+				   (
+					function ( )
+					   {
+						var	$this_button	=  $(this) ;
 
-	function  __initialize_layout ( name ) 
-	   {
-		var	layout		=  layouts [ name ] ;
-		var	template	=  layout. definitions. template ;
-
-		//layout. html		=  __generate_layout ( layout, template ) ;
-
-		layout. definitions. html	=  function ( )
-		   {
-			html	=  '<table class="ui-calculator-content" cellpadding="0" cellspacing="0">' +
-				   '	<tr>' +
-				   '		<td colspan="5">' +
-				   '			<div class="lcd">lcd</div>' +
-				   '		</td>' +
-				   '	</tr>' +
-				   '	<tr>' +
-				   '		<td colspan="3" rowspan="4">' +
-				   '			<table class="group numeric-keypad" cellpadding="0" cellspacing="0">' +
-				   '				<tr>' +
-				   '					<td><div class="button" id="digit_7">Z7</div></td>' +
-				   '					<td><div class="button" id="digit_8">Z8</div></td>' +
-				   '					<td><div class="button" id="digit_9">Z9</div></td>' +
-				   '				</tr> ' +
-				   '				<tr>' +
-				   '					<td><div class="button" id="digit_4">Z4</div></td>' +
-				   '					<td><div class="button" id="digit_5">Z5</div></td>' +
-				   '					<td><div class="button" id="digit_6">Z6</div></td>' +
-				   '				</tr> ' +
-				   '				<tr>' +
-				   '					<td><div class="button" id="digit_1">Z1</div></td>' +
-				   '					<td><div class="button" id="digit_2">Z2</div></td>' +
-				   '					<td><div class="button" id="digit_3">Z3</div></td>' +
-				   '				</tr> ' +
-				   '				<tr>' +
-				   '					<td colspan="2"><div class="button" id="digit_0" colspan="2">Z0</div></td>' +
-				   '					<td class="empty">0e</td>' +
-				   '				</tr> ' +
-				   '			</table>' + 
-				   '		</td>' +
-				   '		<td class="empty">e11</td>' +
-				   '		<td class="empty">e12</td>' +
-				   '	</tr>' + 
-				   '	<tr>' +
-				   '		<td class="empty button-width-1 button-height-1">e21</td>' +
-				   '		<td class="empty button-width-1 button-height-1">e22</td>' +
-				   '	</tr>' +
-				   '	<tr>' +
-				   '		<td class="empty button-width-1 button-height-1">e31</td>' +
-				   '		<td class="empty button-width-1 button-height-1">e32</td>' +
-				   '	</tr>' +
-				   '	<tr>' +
-				   '		<td class="empty button-width-1 button-height-1">e41</td>' +
-				   '		<td class="empty button-width-1 button-height-1">e42</td>' +
-				   '	</tr>' +
-				   '</table>' ;
-
-			return ( html ) ;
-
-		    }
-	    }
+						$this_button. css ( 'height', new_height + 'px' ) ;
+					    }
+				    ) ;
+			    }
+		    ) ;
+	    } ;
 
 
 	/*-------------------------------------------------------------------------------------------------------------
@@ -597,5 +480,5 @@
 	   {
 	    }
 
-    } ( jQuery ) ) ;
+    } ( jQuery, $. script ( ) ) ) ;
 
